be7e0cab09909c3ed1fd7ed4f68a5c41a62eec8e
namber0 <namnb2402@gmail.com>
2023-08-19 19:43:22 +0700

    added an ultrasonic sensor test

---
+++ usensor-test/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+int ledPin = LED_BUILTIN; 
+int trigPin = 10; 
+int echoPin = 9; 
+int duration, distance; 
+
+void setup()
+{
+    pinMode (ledPin, OUTPUT); 
+    pinMode (trigPin, OUTPUT);
+    pinMode (echoPin, INPUT); 
+}
+void loop()
+{
+    digitalWrite (ledPin, LOW); 
+    digitalWrite (trigPin, HIGH);
+    delay(50);
+
+    digitalWrite (trigPin, LOW);
+    duration = pulseIn(echoPin,HIGH);
+    distance = (duration / 2) / 29.1;
+
+    if(distance <= 30) 
+        digitalWrite (ledPin, HIGH);
+    delay(50);
+
+    if(distance >=30)// If no obstacle is there within 30 cm, the Led should turn off.
+        digitalWrite (ledPin, LOW);
+    delay(50);
+    
+    Serial.print("cm");
+    Serial.println(distance);
+}
